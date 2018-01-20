8ea50f1eccdb187b237f9b435c7700647a977288
DESKTOP-MAR0O4V\Dell <andosandor601@gmail.com>
2018-01-20 16:01:01 +0100

    character animation

--- public/js/entities.js
+++ public/js/entities.js
@@ -2,19 +2,28 @@ import Entity from './entity.js';
 import Jump from './traits/Jump.js';
 import Go from './traits/Go.js';
 import { loadSpriteSheet } from './loaders.js';
+import {createAnim} from './anim.js';
 
 export function createMe() {
     return loadSpriteSheet("characters")
         .then(sprite => {
-            console.log(sprite);
             const me = new Entity();
-            me.size.set(16,32);
+            me.size.set(16, 32);
 
             me.addTrait(new Jump());
             me.addTrait(new Go());
 
+            const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 10);
+
+            function routeFrame(me) {
+                if (me.go.dir !== 0) {
+                    return runAnim(me.go.distance);
+                }
+                return 'idle';
+            }
+
             me.draw = function drawMe(context) {
-                sprite.draw('me', context, this.pos.x, this.pos.y);
+                sprite.draw(routeFrame(this), context, this.pos.x, this.pos.y, this.go.heading < 0);
             }
 
             return me;
